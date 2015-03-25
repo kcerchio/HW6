0beff0fff7f596eaed0fd60e286156277bbe71b5
Kyle <kyle-cerchio@uiowa.edu>
2015-03-24 21:25:09 -0500

    functionality good, left off at delete message

--- RecipeDetails.aspx.vb
+++ RecipeDetails.aspx.vb
@@ -2,4 +2,11 @@
 Partial Class RecipeDetails
     Inherits System.Web.UI.Page
 
+    Protected Sub DetailsView1_ItemDeleted(sender As Object, e As DetailsViewDeletedEventArgs) Handles DetailsView1.ItemDeleted
+        Response.AddHeader("REFRESH", "3;URL=./Default.aspx")
+    End Sub
+
+    Protected Sub DetailsView1_ItemUpdated(sender As Object, e As DetailsViewUpdatedEventArgs) Handles DetailsView1.ItemUpdated
+        Response.AddHeader("REFRESH", "3;URL=./Default.aspx")
+    End Sub
 End Class
